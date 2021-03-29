1482de9e1ebd28c9a34d92a3ccea68ed8141c0f5
Andrew Singh <andrew.singh99@gmail.com>
2021-03-29 14:17:29 -0400

    Revised NP to use low-level actions

--- tars/evaluators/metrics_evaluator.py
+++ tars/evaluators/metrics_evaluator.py
@@ -17,17 +17,7 @@ class MetricsEvaluator(Evaluator):
                 env: current environment
         '''
 
-        # Navigation Performance (NP) Metric
-        object_to_navigate_to = MetricsEvaluator.get_object_to_navigate_to(env) # FIXME: see first TODO
-        np = self.navigation_performance_metric(env, object_to_navigate_to)
-        self.episode_metrics["np"] = 1 if np else 0 # for the whole episode (i.e. navigated to the first object it has to interact with
-
-        # Interaction Action Prediction Performance (IAPP) Metric
-        expert_interact_objects, expert_interact_objects_action = MetricsEvaluator.find_objects_to_interact_with(env) # FIXME: see first TODO
-        predicted_action, predicted_mask = "", "" # FIXME pass these from the model
-        iapp = self.iapp_metric(env, expert_interact_objects, expert_interact_objects_action, predicted_action,
-                                          predicted_mask)
-        self.episode_metrics["iapp"] += iapp / len(expert_interact_objects) # percentage of correct actions predicted correctly
+        
 
 
     def at_step_end(self, env, policy_in, policy_out, nrd):
@@ -39,7 +29,15 @@ class MetricsEvaluator(Evaluator):
                 nrd: tuple of (next state, reward, done) after taking executing
                     policy_out
         '''
-        pass
+        # Navigation Performance (NP) Metric
+        if self.episode_metrics['np'] != 1:
+            self.episode_metrics['np'] = int(self.np_metric(env, self.get_np_obj_id(env)))
+
+        # Interaction Action Prediction Performance (IAPP) Metric
+        attempted_interactions = self.get_attempted_interactions(env) # FIXME: see first TODO
+        predicted_action, predicted_mask = "", "" # FIXME pass these from the model
+        iapp = self.iapp_metric(env, attempted_interactions, predicted_action, predicted_mask)
+        self.episode_metrics["iapp"] += iapp / len(attempted_interactions) # percentage of attempted actions predicted correctly
 
     def at_start(self, env, start_state):
         '''
@@ -55,29 +53,26 @@ class MetricsEvaluator(Evaluator):
         '''
         pass
 
-    # Note: object_to_navigate_to is an argument so it is not computed every time
-    def navigation_performance_metric(self, env: AlfredEnv, object_to_navigate_to):
+    # Note: np_obj_id is an argument so it is not computed every time
+    def np_metric(self, env: AlfredEnv, np_obj_id):
         '''
         Assumptions:
 
         How do positions/coordinates work? Assuming positions/coordinates are absolute for whole environment instead of
         a particular scene/image
         '''
-        for object in env.env.last_event.metadata['objects']:
-            if object_to_navigate_to in object['name']:
-                if object['visible']:  # this means that the agent is near the object & object is in its field of view
-                    return True
+        for obj in env.env.last_event.metadata['objects']:
+            if obj['objectId'] == np_obj_id and obj['visible']:
+                return True
         return False
 
 
     # Note: expert_interact_objects, expert_interact_objects_action are arguments so they are not computed every time
-    def iapp_metric(self, env: AlfredEnv, expert_interact_objects, expert_interact_objects_action, predicted_action,
-                    predicted_mask):
+    def iapp_metric(self, env: AlfredEnv, expert_interact_objects, expert_interact_objects_action, predicted_action, predicted_mask):
 
         agent_inter_object = env.env.get_target_instance_id(predicted_mask)
 
-        for expert_inter_object, expert_inter_object_action in zip(expert_interact_objects,
-                                                                   expert_interact_objects_action):
+        for expert_inter_object, expert_inter_object_action in zip(expert_interact_objects, expert_interact_objects_action):
             if agent_inter_object in expert_inter_object and predicted_action == expert_inter_object_action \
                     and (agent_inter_object, predicted_action) not in self.objects_already_interacted_with:
                 self.objects_already_interacted_with.append((agent_inter_object, predicted_action)) # prevent double counting if agent stuck in loop, etc.
@@ -86,7 +81,7 @@ class MetricsEvaluator(Evaluator):
 
 
     @staticmethod
-    def get_object_to_navigate_to(env: AlfredEnv):
+    def get_np_obj_id(env: AlfredEnv):
         '''
         Assumptions:
 
@@ -94,22 +89,24 @@ class MetricsEvaluator(Evaluator):
         expert interacts with in plan, one of the items in the pddl_params
         Assuming the third option (i.e. object_target in pddl_params)
         '''
-        object_to_navigate_to = ""
-        for action in env.high_level_actions:
-            if 'objectId' in action['planner_action']:
-                object_to_navigate_to = action['planner_action']['coordinateObjectId'][0]
-                break
-        return object_to_navigate_to
+        for action in env.low_level_actions:
+            if 'objectId' in action['api_action']:
+                return action['api_action']['objectId']
+        return ""
 
 
     @staticmethod
-    def find_objects_to_interact_with(env: AlfredEnv):
-        interact_objects = []
-        interact_objects_action = []
+    def get_attempted_interactions(env: AlfredEnv):
+        # interact_objects = []
+        # interact_objects_action = []
+        attempted_interactions = []
         for action in env.low_level_actions:
-            if "objectId" in action['api_action']:  # interactions with objects
+            if 'objectId' in action['api_action']:  # interactions with objects
                 objectId = action['api_action']['objectId']
-                interact_objects.append(objectId[:objectId.find('|')])
-                interact_objects_action.append(action['api_action']['action'])
+                attempted_interactions.append(action['api_action'])
+                # interact_objects.append(objectId)
+                # interact_objects_action.append()
+                # interact_objects.append(objectId[:objectId.find('|')])
+                # interact_objects_action.append(action['api_action']['action'])
 
-        return interact_objects, interact_objects_action
+        return attempted_interactions
