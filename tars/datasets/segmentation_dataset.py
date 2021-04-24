f9c5f68bcb1f2d85695b315288cf408959ef2fbb
Ankit Ramchandani <ankitramchandani61@gmail.com>
2021-04-24 12:04:47 -0400

    format change

--- tars/datasets/segmentation_dataset.py
+++ tars/datasets/segmentation_dataset.py
@@ -5,6 +5,7 @@ import numpy as np
 from tars.base.dataset import Dataset
 from PIL import Image
 
+
 class SegmentationDataset(Dataset):
     def __init__(self, type, splits_file=None):
         super().__init__(type, splits_file=splits_file)
