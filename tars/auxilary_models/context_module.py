152e23e384addd2d55770da3a8e8b504c1395f4c
Andrew Singh <andrew.singh99@gmail.com>
2021-04-30 22:30:30 -0400

    Moved embed and readout into its own class for pre-training

--- tars/auxilary_models/context_module.py
+++ tars/auxilary_models/context_module.py
@@ -2,49 +2,40 @@ import torch
 import torch.nn as nn
 from tars.base.model import Model
 from tars.auxilary_models.readout_transformer import ReadoutTransformer
+from tars.auxilary_models.embed_and_readout import EmbedAndReadout
 
 
 class ContextModule(Model):
     def __init__(self, num_actions, num_objects, object_na_idx, conf):
         super(ContextModule, self).__init__()
-        self.action_emb = nn.Embedding(
-                            num_actions + 1, # add one padding action
-                            conf.action_emb_dim,
-                            padding_idx=num_actions
-                        )
 
-        # FIXME: may want to use pretrained word embeddings for objects
-        self.object_emb = nn.Embedding(
-                            num_objects, conf.object_emb_dim,
-                            padding_idx=object_na_idx
-                        )
+        self.action_embed_and_readout = EmbedAndReadout(
+            dict_size=num_actions + 1,
+            embed_dim=conf.action_emb_dim,
+            out_dim=conf.action_hist_emb_dim,
+            padding_idx=num_actions,
+            max_len=conf.past_actions_len,
+            conf=conf
+        )
+
+        self.object_embed_and_readout = EmbedAndReadout(
+            dict_size=num_objects,
+            embed_dim=conf.object_emb_dim,
+            out_dim=conf.int_hist_emb_dim,
+            padding_idx=object_na_idx,
+            max_len=conf.past_objects_len,
+            conf=conf
+        )
 
         self.context_mixer = nn.Linear(
             conf.action_hist_emb_dim + conf.int_hist_emb_dim + conf.inst_hidden_size + conf.goal_hidden_size,
             conf.context_size
         )
 
-        self.action_transformer = ReadoutTransformer(
-                                    in_features=conf.action_emb_dim,
-                                    out_features=conf.action_hist_emb_dim,
-                                    nhead=conf.transformer_num_heads,
-                                    num_layers=conf.transformer_num_layers,
-                                    max_len=conf.past_actions_len
-                                )
-        self.past_object_transformer = ReadoutTransformer(
-                                        in_features=conf.object_emb_dim,
-                                        out_features=conf.int_hist_emb_dim,
-                                        nhead=conf.transformer_num_heads,
-                                        num_layers=conf.transformer_num_layers,
-                                        max_len=conf.past_objects_len
-                                    )
 
     def forward(self, past_actions, past_objects, inst_lstm_cell, goal_lstm_cell):
-        actions = self.action_emb(past_actions).permute(1, 0, 2)
-        objects = self.object_emb(past_objects).permute(1, 0, 2)
-
-        action_readout = self.action_transformer(actions)
-        objects_readout = self.past_object_transformer(objects)
+        action_readout = self.action_embed_and_readout.forward(past_actions)
+        objects_readout = self.object_embed_and_readout.forward(past_objects)
 
         explicit_context = torch.cat((action_readout, objects_readout), dim=1)
         implicit_context = torch.cat((inst_lstm_cell, goal_lstm_cell), dim=1)
