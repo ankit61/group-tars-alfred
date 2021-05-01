b905c466dfb39603cab41062c982237a0f9613fc
Ankit Ramchandani <ankitramchandani61@gmail.com>
2021-05-01 16:28:03 -0400

    make action vocab same across runs

--- tars/config/envs/alfred_env_config.py
+++ tars/config/envs/alfred_env_config.py
@@ -15,9 +15,11 @@ class AlfredEnvConfig(Config):
     stop_action = '<<stop>>'
 
     actions = Vocab(
-                    list(nav_actions) +
-                    list(interact_actions) +
-                    [stop_action]
+                    sorted(
+                        list(nav_actions) +
+                        list(interact_actions) +
+                        [stop_action]
+                    )
                 )
 
     reward_config = os.path.join(str(Path(__file__).parents[2]), 'alfred/models/config/rewards.json')
