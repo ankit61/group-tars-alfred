31cfad8c3cf457372e861d5cb3900a3a26a562c6
Ankit Ramchandani <ankitramchandani61@gmail.com>
2021-05-01 16:31:37 -0400

    put collating code in and mini batch construction code in dataset

--- tars/datasets/imitation_dataset.py
+++ tars/datasets/imitation_dataset.py
@@ -1,15 +1,18 @@
+from collections import defaultdict
 import torch
 import functools
 import numpy as np
+import torch.nn.utils.rnn as rnn_utils
 from tars.base.dataset import Dataset
 
 
 class ImitationDataset(Dataset):
-    def __init__(self, type, img_transforms, text_transforms, splits_file=None):
+    def __init__(self, type, img_transforms, text_transforms, text_collate, splits_file=None):
         super().__init__(type, splits_file=splits_file)
 
         self.img_transforms = img_transforms
         self.text_transforms = text_transforms
+        self.text_collate = text_collate
 
     @functools.lru_cache()
     def __getitem__(self, task_idx):
@@ -43,3 +46,57 @@ class ImitationDataset(Dataset):
 
     def __len__(self):
         return len(self.tasks_json)
+
+    def collate(self, batch):
+        out = {}
+
+        dict_batch = defaultdict(lambda: list())
+        for i in range(len(batch)):
+            for k in batch[i].keys():
+                dict_batch[k].append(batch[i][k])
+
+        # sort by descending length
+        sorted_idxs = np.argsort([-ac.shape[0] for ac in dict_batch['expert_actions']])
+
+        out['expert_actions'] = rnn_utils.pack_sequence(
+                                    list(map(dict_batch['expert_actions'].__getitem__, sorted_idxs))
+                                )
+        out['expert_int_objects'] = rnn_utils.pack_sequence(
+                                    list(map(dict_batch['expert_int_objects'].__getitem__, sorted_idxs))
+                                )
+
+        out['images'] = rnn_utils.pack_sequence(
+                            list(map(lambda i: torch.stack(dict_batch['images'][i]), sorted_idxs))
+                        )
+
+        out['goal_inst'] = self.text_collate(dict_batch['goal_inst'])
+        out['low_insts'] = self.text_collate(dict_batch['low_insts'])
+
+        assert (out['expert_actions'].batch_sizes == out['expert_int_objects'].batch_sizes).all() \
+            and (out['expert_actions'].batch_sizes == out['images'].batch_sizes).all()
+
+        out['batch_sizes'] = out['expert_actions'].batch_sizes
+
+        return out
+
+    @staticmethod
+    def mini_batches(batch):
+        '''
+            Given a batch of tasks, loop over them in sequence to get parallel
+            batches in order
+        '''
+
+        start = 0
+        for bsz in batch['batch_sizes']:
+            mini_batch = {}
+
+            mini_batch['expert_actions'] = batch['expert_actions'].data[start:start+bsz]
+            mini_batch['expert_int_objects'] = batch['expert_int_objects'].data[start:start+bsz]
+            mini_batch['images'] = batch['images'].data[start:start+bsz]
+
+            mini_batch['goal_inst'] = batch['goal_inst']
+            mini_batch['low_insts'] = batch['low_insts']
+
+            yield mini_batch, bsz
+
+            start += bsz
