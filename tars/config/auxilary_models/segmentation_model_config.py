3950c59105617440b5c495d7c7fa457c2d4b9f65
Ankit Ramchandani <ankitramchandani61@gmail.com>
2021-05-01 08:23:20 +0000

    reduce batch size

--- tars/config/auxilary_models/segmentation_model_config.py
+++ tars/config/auxilary_models/segmentation_model_config.py
@@ -3,7 +3,7 @@ from tars.base.config import Config
 
 
 class SegmentationModelConfig(Config):
-    batch_size = 32
+    batch_size = 16
 
     def get_optim(self, parameters):
         return optim.Adam(parameters, lr=1e-3)
