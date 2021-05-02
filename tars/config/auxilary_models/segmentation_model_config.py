7c222f42066308cafa785a39131371cec37b3fe4
Ankit Ramchandani <ankitramchandani61@gmail.com>
2021-05-01 22:28:55 -0400

    inherit seg from model

--- tars/config/auxilary_models/segmentation_model_config.py
+++ tars/config/auxilary_models/segmentation_model_config.py
@@ -1,8 +1,8 @@
 import torch.optim as optim
-from tars.base.config import Config
+from tars.config.base.model_config import ModelConfig
 
 
-class SegmentationModelConfig(Config):
+class SegmentationModelConfig(ModelConfig):
     batch_size = 16
 
     def get_optim(self, parameters):
