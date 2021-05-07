e59b0dbe2aacf500a941c660f75dcb1b3ffc8e6d
Ankit Ramchandani <ankitramchandani61@gmail.com>
2021-05-07 00:44:02 -0400

    add threshold

--- tars/config/auxilary_models/multi_label_classifier_config.py
+++ tars/config/auxilary_models/multi_label_classifier_config.py
@@ -4,6 +4,7 @@ from tars.config.base.model_config import ModelConfig
 
 class MultiLabelClassifierConfig(ModelConfig):
     batch_size = 64
+    pred_threshold = 0.5
 
     def get_optim(self, parameters):
         return optim.Adam(parameters)
