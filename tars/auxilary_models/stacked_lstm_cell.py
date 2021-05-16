83141f6535f3cc9dc2c5fcafa2ce59053f4d9944
Ankit Ramchandani <ankitramchandani61@gmail.com>
2021-05-15 20:02:17 -0400

    normalize after every lstm layer

--- tars/auxilary_models/stacked_lstm_cell.py
+++ tars/auxilary_models/stacked_lstm_cell.py
@@ -5,6 +5,8 @@ from tars.base.model import Model
 class StackedLSTMCell(Model):
     def __init__(self, input_size, hidden_size, num_layers):
         super(StackedLSTMCell, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.lstm_cells = nn.ModuleList([
             nn.LSTMCell(
@@ -12,6 +14,10 @@ class StackedLSTMCell(Model):
                 hidden_size=hidden_size
             ) for i in range(num_layers)
         ])
+        self.norm_layers = nn.ModuleList([
+                            nn.LayerNorm([hidden_size]) if i < num_layers - 1 else nn.Sequential() # don't normalize in the end
+                            for i in range(num_layers)
+                        ])
 
     def forward(self, x, past_hiddens_cells=None):
         # FIXME: add skip connections
@@ -21,7 +27,7 @@ class StackedLSTMCell(Model):
         cur_in = x
         for i in range(len(self.lstm_cells)):
             out = self.lstm_cells[i](cur_in, past_hiddens_cells[i])
-            cur_in = out[0]
+            cur_in = self.norm_layers[i](out[0])
             hiddens_cells.append(out)
 
         return hiddens_cells
